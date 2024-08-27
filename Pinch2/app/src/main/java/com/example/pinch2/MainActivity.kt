a33be7e67eafd95ba6630e8d409e95acb6a65d6c
joshlasota <142518176+joshlasota@users.noreply.github.com>
2024-08-27 11:00:24 -0500

    Add files via upload

---
+++ Pinch2/app/src/main/java/com/example/pinch2/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.pinch2
+
+import android.os.Bundle
+import android.view.MotionEvent
+import android.view.ScaleGestureDetector
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+
+lateinit var iv: ImageView
+var scale = 1f
+
+class MainActivity : AppCompatActivity() {
+
+    var ourSD: ScaleGestureDetector? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        iv = findViewById(R.id.idImg)
+        ourSD = ScaleGestureDetector(this, ScaleListener())
+
+    }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        ourSD!!.onTouchEvent(event)
+        return super.onTouchEvent(event)
+    }
+
+    class ScaleListener: ScaleGestureDetector.SimpleOnScaleGestureListener(){
+        override fun onScale (detector: ScaleGestureDetector): Boolean {
+
+            scale *= detector.scaleFactor
+            scale = Math.max(0.1f, Math.min(scale, 5.0f))
+            iv.scaleX = scale
+            iv.scaleY = scale
+
+            return true
+        }
+    }
+
+}
